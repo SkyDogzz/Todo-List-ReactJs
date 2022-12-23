90b21fe7feb624509c420406ee637d9fa99470a9
SkyDogzz <thomas.stephan@live.fr>
2022-12-23 12:12:32 +0100

    Add id to handleClick function

--- src/components/TodoListItems.js
+++ src/components/TodoListItems.js
@@ -2,11 +2,16 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 
 export default function TodoListItems(props) {
-  const handleClick = (event) => {
-    console.log(event.target);
+  const handleClick = (id) => {
+    console.log(id);
   };
   const items = props.items.map((item) => (
-    <TodoListItem key={item.id} content={item.content} done={item.done} handleClick={handleClick}/>
+    <TodoListItem
+      key={item.id}
+      content={item.content}
+      done={item.done}
+      handleClick={() => handleClick(item.id)}
+    />
   ));
   return (
     <div>
