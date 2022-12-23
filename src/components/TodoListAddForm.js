c12162eca9a14d0d6565e1ddbf06bb03181bf098
SkyDogzz <thomas.stephan@live.fr>
2022-12-23 15:40:52 +0100

    Add className

--- src/components/TodoListAddForm.js
+++ src/components/TodoListAddForm.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TodoListAddForm(props) {
   return (
-    <div>
+    <div className="TodoListAddForm">
       <form onSubmit={props.handleAdd}>
         <input type="text" />
         <button type="submit">Ajouter</button>
